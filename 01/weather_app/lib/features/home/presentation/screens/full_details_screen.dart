5afd828670d1659856dd71c8715ebb1a8e5600e8
7Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2025-02-19 20:52:34 +0200

    Impl background color based on temperature

--- 01/weather_app/lib/features/home/presentation/screens/full_details_screen.dart
+++ 01/weather_app/lib/features/home/presentation/screens/full_details_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/core/extensions/double_extension.dart';
 import 'package:weather_app/features/home/presentation/components/city_data_view.dart';
 import 'package:weather_app/features/home/presentation/components/error_page.dart';
 import 'package:weather_app/features/home/presentation/controller/bloc/weather_bloc.dart';
@@ -8,13 +9,26 @@ import 'package:weather_app/features/home/presentation/controller/bloc/weather_b
 class FullDetailsScreen extends StatelessWidget {
   const FullDetailsScreen({super.key});
 
+  Color backgroundColor(int tempreture) {
+    if (tempreture < 10) {
+      return Colors.blue;
+    } else if (tempreture < 35) {
+      return Colors.green;
+    } else {
+      return Colors.red;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final backgroundColor = Colors.red.withAlpha(25);
     return BlocBuilder<WeatherBloc, WeatherState>(
       builder: (context, state) {
         return Scaffold(
-          backgroundColor: backgroundColor,
+          backgroundColor: switch (state) {
+            WeatherLoadedState s =>
+              backgroundColor(s.weatherEntity.temperature.kelvinToCelsius()),
+            _ => null,
+          },
           appBar: AppBar(
               backgroundColor: Colors.transparent,
               title: const Text("Full Details"),
@@ -31,6 +45,7 @@ class FullDetailsScreen extends StatelessWidget {
                         ),
                         Text(
                           s.weatherEntity.cityName,
+                          style: Theme.of(context).textTheme.titleLarge,
                         ),
                       ],
                     ),
