2b208be9d5321d0ca2c3ca04862b4eecae6bb8b6
7Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2025-02-19 20:01:06 +0200

    Wire FullDetailsScreen with WeatherBLoc

--- 01/weather_app/lib/features/home/presentation/screens/full_details_screen.dart
+++ 01/weather_app/lib/features/home/presentation/screens/full_details_screen.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:weather_app/features/home/presentation/components/weather_info_card.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/features/home/presentation/components/city_data_view.dart';
+import 'package:weather_app/features/home/presentation/controller/bloc/weather_bloc.dart';
 
 class FullDetailsScreen extends StatelessWidget {
   const FullDetailsScreen({super.key});
@@ -8,53 +10,43 @@ class FullDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final backgroundColor = Colors.red.withAlpha(25);
-    return Scaffold(
-      backgroundColor: backgroundColor,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: const Text("Full Details"),
-        centerTitle: true,
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(50),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            spacing: 20,
-            children: [
-              Icon(
-                Icons.location_on,
+    return BlocBuilder<WeatherBloc, WeatherState>(
+      builder: (context, state) {
+        return Scaffold(
+          backgroundColor: backgroundColor,
+          appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              title: const Text("Full Details"),
+              centerTitle: true,
+              bottom: switch (state) {
+                WeatherLoadedState _ => PreferredSize(
+                    preferredSize: const Size.fromHeight(kToolbarHeight),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      spacing: 20,
+                      children: [
+                        Icon(
+                          Icons.location_on,
+                        ),
+                        Text(
+                          "London",
+                        ),
+                      ],
+                    ),
+                  ),
+                _ => null,
+              }),
+          body: switch (state) {
+            WeatherLoadedState s => CityDataView(
+                fetchedWeather: s.weatherEntity,
+                cachedWeather: s.weatherEntity,
               ),
-              Text(
-                "London",
-              ),
-            ],
-          ),
-        ),
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(30),
-        children: [
-          WeatherInfoCard(
-            title: "Temperature",
-            iconData: Icons.sunny,
-            newValue: "45 °C",
-            oldValue: "45 °C",
-          ),
-          SizedBox(height: 15),
-          WeatherInfoCard(
-            title: "Humidity",
-            iconData: Icons.water,
-            newValue: "67%",
-            oldValue: "89%",
-          ),
-          SizedBox(height: 15),
-          WeatherInfoCard(
-            title: "Wind Speed",
-            iconData: Icons.wind_power_sharp,
-            newValue: "4.12",
-            oldValue: "6.7",
-          ),
-        ],
-      ),
+            WeatherErrorState s => Center(child: Text(s.msg)),
+            WeatherInitialState _ => SizedBox(),
+            WeatherLoadingState _ => Center(child: CircularProgressIndicator()),
+          },
+        );
+      },
     );
   }
 }
