25d8cff32e29562ed1536f42a99edc9a1c0b387a
7Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2025-02-19 17:51:20 +0200

    dependency injection
    inject all repos and data sources etc

---
+++ 01/weather_app/lib/core/di/dependency_injection.dart
@@ -0,0 +1,21 @@
+import 'package:get_it/get_it.dart';
+import 'package:weather_app/features/home/data/data_source/weather_remote_data_source.dart';
+import 'package:weather_app/features/home/data/repository/weather_repo.dart';
+import 'package:weather_app/features/home/domain/use_cases/get_weather_details.dart';
+import 'package:weather_app/features/home/presentation/controller/bloc/weather_bloc.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initSL() async {
+  ///Init DataSources
+  sl.registerLazySingleton(() => WeatherRemoteDataSource());
+
+  ///Init Repos
+  sl.registerLazySingleton(() => WeatherRepo(sl()));
+
+  ///Init usecases
+  sl.registerLazySingleton(() => GetWeatherDetailsUseCase(sl()));
+
+  ///Init blocs and cubits
+  sl.registerFactory(() => WeatherBloc(sl()));
+}
