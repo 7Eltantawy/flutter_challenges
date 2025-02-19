302398a56682911850ca7ae80ffdb463ed14fdb7
7Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2025-02-19 18:25:44 +0200

    define global navigatorKey

--- 01/weather_app/lib/app.dart
+++ 01/weather_app/lib/app.dart
@@ -4,10 +4,14 @@ import 'package:weather_app/features/home/presentation/screens/home_screen.dart'
 class WeatherApp extends StatelessWidget {
   const WeatherApp({super.key});
 
+  static final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey<NavigatorState>();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Weather App',
+      navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
