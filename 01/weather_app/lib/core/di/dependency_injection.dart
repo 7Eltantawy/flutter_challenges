841fac6d7b85d5a62180514ab5e8265d44f86278
7Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2025-02-19 18:33:27 +0200

    Fix registered type of implemented classes

--- 01/weather_app/lib/core/di/dependency_injection.dart
+++ 01/weather_app/lib/core/di/dependency_injection.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:weather_app/features/home/data/data_source/weather_remote_data_source.dart';
 import 'package:weather_app/features/home/data/repository/weather_repo.dart';
+import 'package:weather_app/features/home/domain/repository/base_weather_repo.dart';
 import 'package:weather_app/features/home/domain/use_cases/get_weather_details.dart';
 import 'package:weather_app/features/home/presentation/controller/bloc/weather_bloc.dart';
 
@@ -8,10 +9,11 @@ final sl = GetIt.instance;
 
 Future<void> initSL() async {
   ///Init DataSources
-  sl.registerLazySingleton(() => WeatherRemoteDataSource());
+  sl.registerLazySingleton<BaseWeatherRemoteDataSource>(
+      () => WeatherRemoteDataSource());
 
   ///Init Repos
-  sl.registerLazySingleton(() => WeatherRepo(sl()));
+  sl.registerLazySingleton<BaseWeatherRepo>(() => WeatherRepo(sl()));
 
   ///Init usecases
   sl.registerLazySingleton(() => GetWeatherDetailsUseCase(sl()));
