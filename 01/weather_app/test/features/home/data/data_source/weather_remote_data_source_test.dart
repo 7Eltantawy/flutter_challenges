fdd147e6dc7b7e1adb705053e78dbe5760875db9
7Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2025-02-20 08:53:19 +0200

    Write a test for WeatherRemoteDataSource

---
+++ 01/weather_app/test/features/home/data/data_source/weather_remote_data_source_test.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:weather_app/core/constants/const.dart';
+import 'package:weather_app/core/error/exceptions.dart';
+import 'package:weather_app/core/network/api_error_msg_model.dart';
+import 'package:weather_app/features/home/data/data_source/weather_remote_data_source.dart';
+import 'package:weather_app/features/home/domain/entities/weather_entity.dart';
+
+import '../../../../dummy/weather_api_response.dart';
+import 'weather_remote_data_source_test.mocks.dart';
+
+// class DioMock extends Mock implements Dio {}
+
+@GenerateMocks([Dio])
+void main() {
+  late MockDio dioMock;
+  late WeatherRemoteDataSource weatherRemoteDataSource;
+
+  setUp(() {
+    dioMock = MockDio();
+    weatherRemoteDataSource = WeatherRemoteDataSource(dioMock);
+  });
+
+  group('Get weather details', () {
+    final cityName = 'Cairo';
+    final queryParams = {
+      'q': cityName,
+      'appid': kApiKey,
+    };
+
+    test('Should return WeatherEntity when the response code is 200', () async {
+      // ARRANGE
+      final responseData = jsonDecode(dummyApiResponse);
+      final response = Response(
+        requestOptions: RequestOptions(path: ''),
+        statusCode: 200,
+        data: responseData,
+      );
+      when(dioMock.get(kApiBaseUrl, queryParameters: queryParams))
+          .thenAnswer((_) async => Future.value(response));
+
+      // ACT
+      final result = await weatherRemoteDataSource.getWeatherDetails(cityName);
+
+      // ASSERT
+      expect(result, isA<WeatherEntity>());
+    });
+
+    test('Should throw ServerException when the response code is not 200',
+        () async {
+      // ARRANGE
+      final ApiErrorMsgModel apiErrorMsgModel = ApiErrorMsgModel(
+        statusCode: 500,
+        statusMsg: "statusMsg",
+        success: false,
+      );
+      final responseData = apiErrorMsgModel.toMap();
+      final response = Response(
+        requestOptions: RequestOptions(path: ''),
+        statusCode: 500,
+        data: responseData,
+      );
+      when(dioMock.get(kApiBaseUrl, queryParameters: queryParams))
+          .thenAnswer((_) async => Future.value(response));
+
+      // ACT
+      final result = weatherRemoteDataSource.getWeatherDetails(cityName);
+
+      // ASSERT
+      expect(result, throwsA(isA<ServerException>()));
+    });
+  });
+}
